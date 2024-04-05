7ccd17f05ab325cd00e2df68078ea22eaf73ddf9
Jan Nordskog <jan.nordskog@gmail.com>
2024-04-05 16:21:55 +0200

    revert

--- operativ-3/src/main/java/no/ntnu/server/ServerStart.java
+++
@@ -1,9 +0,0 @@
-package no.ntnu.server;
-
-public class ServerStart {
-    public static void main(String[] args) {
-        ServerLogic logic = new ServerLogic();
-        Server server = new Server(logic, true);
-        server.start();
-    }
-}
