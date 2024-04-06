da6ed82f36484ab672af1dbace9433e6fdfb2cc3
runemola <runemola@stud.ntnu.no>
2024-04-06 19:31:07 +0200

    Check

--- src/main/java/no/ntnu/Main.java
+++ src/main/java/no/ntnu/Main.java
@@ -5,6 +5,6 @@ import no.ntnu.server.ServerLogic;
 public class Main {
     public static void main(String[] args) {
         ServerLogic logic = new ServerLogic();
-        System.out.println(logic.getResult("A10 5, S5 4, M7 6, D9 8").toString());
+        System.out.println(logic.getResult("S5 4, M7 6, D9 8").toString());
     }
 }
